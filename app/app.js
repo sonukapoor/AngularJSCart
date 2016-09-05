64ab5ea9f72bb2ea1149b527bb7589b13b48fbf0
Sonu Kapoor <sonu@solidsoftwaresolutions.ca>
2016-09-05 11:03:03 -0400

    first commit

---
+++ app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('CartApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]);
+
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
