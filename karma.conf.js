1b9e53841fdb9fc40be56c62c183a6cedfd944ad
Sonu Kapoor <sonu@solidsoftwaresolutions.ca>
2016-09-13 14:13:02 -0400

    removed istanbul integration from the package.json and added into the karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config)
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/app/app.js': ['coverage'],
+      'app/components/**/*.js': ['coverage'],
       'app/services/**/*.js': ['coverage']
     },
 
